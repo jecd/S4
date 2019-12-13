4cfc2372d559edb0342138a8930d4a99036393b5
rashecl <31122647+rashecl@users.noreply.github.com>
2019-12-13 08:49:37 -0800

    updated S4fcns

--- S4fcns.py
+++ S4fcns.py
@@ -14,10 +14,12 @@ conn_string = ("host={} port={} dbname={} user={} password={}") \
         .format(PGHOST, PGPORT, PGDATABASE, PGUSER, PGPASSWORD)
 conn=psycopg2.connect(conn_string)
 
+sql_command = "SELECT page_ark FROM {} group by page_ark;".format("rtesseract_words")
+pageIDs = pd.read_sql(sql_command, conn)
 
-def colorDoc(documentID = 'd7pp4q-027', colorLabels = [], my_dpi = 220.53, colorBy = 'confidence'):
-    sql_command = "SELECT * FROM {} WHERE page_ark = '{}';".format("rtesseract_words",documentID)
-    df = pd.read_sql(sql_command, conn)
+def colorDoc(document = 'd7pp4q-027', colorLabels = [], my_dpi = 220.53, colorBy = 'confidence'):
+    df = getDoc(document)
+    # Format dataframe from sql output
     df['text'] = df['text'].str.lower()
     def roundup(x):
         return x if x % 100 == 0 else x + 100 - x % 100
@@ -25,7 +27,6 @@ def colorDoc(documentID = 'd7pp4q-027', colorLabels = [], my_dpi = 220.53, color
     im_width = roundup(max(df.right))
     df.top = im_height-df.top
     df.bottom = im_height-df.bottom
-    
     # Deterimine coloring scheme
     # If appropriate sized color labels are supplied, then use those. Otherwise go with default (i.e. confidence).
     if len(colorLabels) > 0:
@@ -48,7 +49,12 @@ def colorDoc(documentID = 'd7pp4q-027', colorLabels = [], my_dpi = 220.53, color
         confidences = list(range(101))
         cmap = plt.get_cmap('RdYlGn')(np.linspace(0.15, 0.85, len(confidences)))
         colorLabels = df['confidence'].astype(int)
-    
+    elif colorBy == 'text_confidence':
+        confidences = list(range(101))
+        df['confidence'][df['confidence']<60] = 0
+        cmap = plt.get_cmap('RdYlGn')(np.linspace(0.15, 0.85, len(confidences)))
+        colorLabels = df['confidence'].astype(int)
+        colorLabels = df['confidence'].astype(int)
     # Plot:
     fig, ax = plt.subplots(figsize=(im_width/my_dpi, im_height/my_dpi), dpi=my_dpi)
     for idx,word,c in zip(range(len(df)),df.text,colorLabels):
@@ -57,7 +63,21 @@ def colorDoc(documentID = 'd7pp4q-027', colorLabels = [], my_dpi = 220.53, color
         width = df.right[idx]-df.left[idx]
         ax.add_patch(mpl.patches.Rectangle(start, width = width, height = height, color = cmap[c,:], label = colorBy + '=' + str(c) ))
         ax.annotate(word,start, color='k', weight='bold', 
-                    fontsize=height/6, ha='left', va='bottom')
+                    fontsize=height/4, ha='left', va='bottom')
         ax.set_xlim((0,im_width))
         ax.set_ylim((0,im_height))
+    plt.title(str(docIdx) + ' : ' + documentName,fontdict = {'fontsize':22})
     plt.show()
+    return df
+
+def getDoc(document): 
+    # Parse args
+    if type(document) == int:
+        documentName = pageIDs.page_ark[document]
+        docIdx = document
+    else:
+        documentName = document
+        docIdx = pageIDs[pageIDs['page_ark']==documentName].index[0]
+    sql_command = "SELECT * FROM {} WHERE page_ark = '{}';".format("rtesseract_words",documentName)
+    df = pd.read_sql(sql_command, conn)
+    return df
